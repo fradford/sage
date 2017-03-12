ce75a9a3dfd7c0d2ba41d51ebb292f75cc1cdd78
FRadford <francis.radford@outlook.com>
2017-03-11 23:34:15 -0500

    Timer class optimizations

--- src/Timer.py
+++ src/Timer.py
@@ -1,5 +1,6 @@
 import time
 
+
 class Timer:
     def __enter__(self):
         self.start = time.time()
@@ -10,8 +11,8 @@ class Timer:
 
         self.elapsed = self.end - self.start
         if self.elapsed > 3600:
-            self.elapsed = str(self.elapsed/3600) + " hours."
+            self.elapsed = str(self.elapsed / 3600) + " hours."
         elif 60 < self.elapsed < 3600:
-            self.elapsed = str(self.elapsed/60) + " minutes."
+            self.elapsed = str(self.elapsed / 60) + " minutes."
         else:
             self.elapsed = str(self.elapsed) + " seconds."
